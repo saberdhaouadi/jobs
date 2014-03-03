af359bbc555a859344fb74f8f30200ac64b7868e
Rob Vermaas <rob.vermaas@logicblox.com>
2014-03-03 14:24:44 +0100

    Support 3.10 and 4.0.

--- worker/nix/job.nix
+++ worker/nix/job.nix
@@ -1,34 +1,37 @@
-{ name
-, job
-, platform_version ? "4.0.7"
-, input ? "/tmp/in"
-, output ? "/tmp/out"
+{ platform_version ? "3.10.9"
 }:
 let
   inherit (import <config/lib> {}) releases version buildLB pkgs;
   platform = builtins.getAttr platform_version releases.platform;
 in
   pkgs.stdenv.mkDerivation {
-    inherit name;
+    name = "job-${toString builtins.currentTime}";
     buildInputs = [
       platform.logicblox
       platform.bloxweb
+      releases.pdxscience."4.0.0".pdxscience
     ];
     buildCommand = ''
       echo ""
       echo "starting LogicBlox services"
-      lb services start &> /dev/null
+      if which lb-services &> /dev/null ; then
+        lb-services start &> /dev/null
+      else
+        lb services start &> /dev/null
+      fi
 
-      tar --strip-components=1 -xf ${job}
+      tar --strip-components=1 -xf /tmp/job/job.tar.gz
 
       echo ""
       echo "running job"
       if [[ -f ./run ]]; then
-        bash run
+        bash run /tmp/job/in /tmp/job/out
       else
         echo "ERROR: run.sh not found!"
       fi
 
+      rm -rf $out
       mkdir -p $out
+      chmod -R 777 . /tmp/job/out/*
     '';
   }
