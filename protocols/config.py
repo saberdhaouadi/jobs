8dbeb7d871a6c2afeeac7df8c050c0893c7d31b9
Rob Vermaas <rob.vermaas@logicblox.com>
2014-03-04 13:07:35 +0100

    Add protocol library

---
+++ protocols/config.py
@@ -0,0 +1,30 @@
+from lbconfig.api import *
+
+lbconfig_package(
+  'lb-steve-protocols',
+  version='1.0',
+  default_prefix='/opt/logicblox/lb-steve-protocols',
+  default_targets=['jars'])
+
+depends_on(
+  logicblox_dep
+)
+
+protobuf_protocol(
+  name = 'backend',
+  package = 'lb.steve',
+  java_package = 'com.logicblox.steve.protocol',
+  srcdir = 'proto',
+  gen_datalog=False
+)
+
+jar(
+   name = 'lb-steve-protocols',
+   srcdir = 'java',
+   srcgen = [
+      java_protobuf_file('backend', 'com.logicblox.steve.protocol'),
+   ],
+   classpath = [
+      '$(logicblox)/lib/java/protobuf-2.5.0.jar'
+   ])
+
