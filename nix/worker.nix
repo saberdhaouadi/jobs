1d98536ef4c1fffd6023af51c5adad5a7d94ec0d
Rob Vermaas <rob.vermaas@logicblox.com>
2014-03-18 14:08:29 +0100

    Set hostname to instance id and notify syslog.

--- nix/worker.nix
+++ nix/worker.nix
@@ -48,7 +48,28 @@ let
         };
       };
 
-      networking.hostName = "worker";
+      networking.hostName = pkgs.lib.mkForce "";
+
+      systemd.services.set-hostname =
+        { description = "Set hostname to instance-id";
+
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+
+          path = [ pkgs.curl pkgs.coreutils pkgs.nettools ];
+
+          script =
+            ''
+              hostname $(curl -s --retry 3 --retry-delay 10 -m 30 http://169.254.169.254/latest/meta-data/instance-id)
+              if [[ -f /var/run/rsyslogd.pid ]]; then
+                kill -HUP `cat /var/run/rsyslogd.pid`
+              fi
+            '';
+
+          serviceConfig.Type = "oneshot";
+          serviceConfig.RemainAfterExit = true;
+        };
+
       time.timeZone = "UTC";
     };
 in
