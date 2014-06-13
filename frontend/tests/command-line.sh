ab36bbbd3883a6c60678d35a857ee3b45797bbf4
Martin Bravenboer <martin.bravenboer@logicblox.com>
2014-06-12 21:16:13 -0700

    Setting up simple end-to-end testsuite that currently requires network access

---
+++ frontend/tests/command-line.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+set -u
+set -x
+
+scriptdir=$(readlink -f $(dirname $BASH_SOURCE))
+topdir=$scriptdir/../..
+
+function start_servers()
+{
+  killall java || echo "failure okay"
+
+  lb-steve-frontend --config ./frontend.config &
+  frontend_pid=$!
+  # pure evil, but okay
+  sleep 3
+
+  export NIX_PATH=worker=$topdir/worker-minimal:$NIX_PATH
+  lb-steve-worker --incoming $(cat $scriptdir/frontend.config | awk '$1 == "sqs_queue_url" {print $3}' | sed '1q;d') \
+                  --outgoing $(cat $scriptdir/frontend.config | awk '$1 == "sqs_queue_url" {print $3}' | sed '2q;d') &
+  worker_pid=$!
+}
+
+function stop_servers()
+{
+  kill $frontend_pid
+  kill $worker_pid
+}
+
+start_servers
+trap stop_servers EXIT
+
+tar czvf total.tar.gz -C $topdir/sample-jobs total
+lb-steve-client upload-impl --impl total-v1 -i total.tar.gz
