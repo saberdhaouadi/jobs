a480cbb7436961047dd5f116e60a11cba8a6a8d9
Rob Vermaas <rob.vermaas@logicblox.com>
2014-07-22 09:10:30 +0000

    Update image

--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -19,7 +19,7 @@ public class Main
 
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-ce70b4a6";
+  private static String ami = "ami-6a1cd202";
   private static String key = "rob";
   private static String s3Bucket = "steve-jobs";
   private static List<String> attrs = Arrays.asList("ApproximateNumberOfMessages", "ApproximateNumberOfMessagesNotVisible");
