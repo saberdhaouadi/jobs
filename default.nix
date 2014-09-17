9cc3bb7677fff0940eb6abf758ce10f902aef755
Rob Vermaas <rob.vermaas@logicblox.com>
2014-09-17 16:13:02 +0200

    Use default jdk (openjdk).

--- default.nix
+++ default.nix
@@ -6,7 +6,6 @@ let
   builder_config = import <config> {};
   inherit (builder_config) pkgs getPlatform;
   platform = getPlatform platform_release;
-  jdk7_jce = pkgs.oraclejdk7.override (a: { installjce = true; }) ;
 
   s3lib =
     let jobs = import src_s3lib { s3lib = src_s3lib; };
@@ -17,7 +16,6 @@ in
     inherit src;
     logicblox = platform.logicblox;
     lb_web = platform.bloxweb;
-    jdk = jdk7_jce;
     inherit s3lib builder_config;
-    inherit (pkgs) python stdenv fetchurl unzip makeWrapper runCommand;
+    inherit (pkgs) python stdenv fetchurl unzip makeWrapper runCommand jdk;
   }
