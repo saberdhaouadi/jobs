9124fcfa41db8be19e50cbd61dcb6be054a7a595
Rob Vermaas <rob.vermaas@logicblox.com>
2014-09-24 11:16:50 +0200

    Enable default-signature authentication for the frontend.

--- frontend/java/com/logicblox/steve/Main.java
+++ frontend/java/com/logicblox/steve/Main.java
@@ -25,6 +25,8 @@ import com.logicblox.common.logging.SystemDLevel;
 import com.logicblox.common.logging.SystemDLogger;
 import org.apache.commons.cli.*;
 
+import com.logicblox.bloxweb.internal.Specification;
+
 public class Main
 {
   public static void main(String[] args)
@@ -46,6 +48,10 @@ public class Main
         ConfigValidator.handleMessages(messages, logger);
 
         main._ctx = ServiceContext.fromConfig(main._config, main._logger);
+
+        // Create job-auth realm for authentication
+        Specification.Realm.Builder realm = Specification.Realm.newBuilder().setName("job-auth").setConfig("default-signature");
+        main._ctx.getAuthenticationProvider().addRealm(realm.build());
 
         final BloxWebServer bloxwebServer = new BloxWebServer(
           Option.some(main._logDir),
