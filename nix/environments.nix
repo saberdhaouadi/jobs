a76a68e248810ffb57c07b630514d73171a9f82a
Rob Vermaas <rob.vermaas@logicblox.com>
2014-10-02 16:54:07 +0200

    Add elastic ips/environments for martin and thiago

--- nix/environments.nix
+++ nix/environments.nix
@@ -10,4 +10,18 @@
       elasticIPv4 = "23.21.124.192";
       workers = { "c3.xlarge" = { number = 1; price = "0.25"; }; };
     };
+
+  thiago =
+    { hostName = "steve-thiago.logicblox.com";
+      elasticIPv4 = "54.163.249.223";
+      workers = { "c3.xlarge" = { number = 1; price = "0.25"; }; };
+    };
+
+  thiago =
+    { hostName = "steve-martin.logicblox.com";
+      elasticIPv4 = "54.163.249.223";
+      workers = { "c3.xlarge" = { number = 1; price = "0.25"; }; };
+    };
+
+
 }
