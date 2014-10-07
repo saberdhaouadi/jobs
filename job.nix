7658c72d985bb72e52a1b212538dc5206c3ee52d
Rob Vermaas <rob.vermaas@logicblox.com>
2014-10-07 11:02:27 +0200

    Add client.build to deps of frontend to get consistent lb-steve on nix-shell.

--- job.nix
+++ job.nix
@@ -96,7 +96,7 @@ rec {
      builder_config.buildLBConfig {
       name = "jobs-frontend-${version src}";
       src = "${src}/frontend";
-      buildInputs = [ logicblox lb_web makeWrapper ];
+      buildInputs = [ logicblox lb_web makeWrapper client.build ];
       enableLBservices = false;
       configureFlags = [
         "--with-protocols=${protocols}"
