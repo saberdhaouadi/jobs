95be283726eeb7017f02f45eac43b20589672406
Rob Vermaas <rob.vermaas@logicblox.com>
2014-10-20 12:14:52 +0000

    Fix paths in installer script

--- frontend-database/scripts/install.sh
+++ frontend-database/scripts/install.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
+set -e
 
+top=$(cd $(dirname $0); pwd)
 lb create --overwrite lb-steve
-lb addproject lb-steve $(cd $(dirname $0); pwd)/share/lb_steve_frontend_database --libpath $LB_WEBSERVER_HOME
+lb addproject lb-steve $top/share/lb_steve_frontend_database --libpath $LB_WEBSERVER_HOME:$top/share
 lb web-server load-services
