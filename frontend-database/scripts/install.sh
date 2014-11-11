3a68263aa39d6c4b12f994292dc4db0b0dd7ef37
Rob Vermaas <rob.vermaas@logicblox.com>
2014-11-11 14:28:12 +0000

    Export workspace in install.sh. Remove any failed lb-steve workspace in case of install.sh failure, to prevent correupt data export on next deployment.

--- frontend-database/scripts/install.sh
+++ frontend-database/scripts/install.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 set -e
 
+function trap_handler() {
+  echo "Removing failed lb-steve workspace"
+  lb delete lb-steve || true
+}
+
 top=$(cd $(dirname $0); pwd)
 
 tdx="users jobimpls jobimpl_metadata jobs job_metadata job_inputs job_outputs job_status provision-config"
@@ -8,6 +13,7 @@ backup_dir="$LB_DEPLOYMENT_HOME/exports/`date +"%Y%m%d-%H%M%S"`"
 latest_link=$LB_DEPLOYMENT_HOME/exports/latest
 
 if [[ -d "$(lb filepath lb-steve)" ]]; then
+  trap trap_handler ERR
 
   mkdir -p $backup_dir
   echo "Export data to $backup_dir"
@@ -16,6 +22,7 @@ if [[ -d "$(lb filepath lb-steve)" ]]; then
     lb web-client export -n -o file://$backup_dir/$t.csv http://localhost:8080/tdx/$t
   done
 
+  lb export lb-steve $backup_dir/workspace
   rm -f $latest_link
   ln -s $backup_dir $latest_link
 fi
