e03034c0d15a922ccac01ad7a0e76324d29ca232
Rob Vermaas <rob.vermaas@logicblox.com>
2014-11-26 09:44:15 +0000

    Make metrics more generic, will move this code into separate library and nix module, as it might be convenient for other projects as well, especially when people use Datadog.

--- nix/datadog/lb-steve-database.py
+++ nix/datadog/lb-steve-database.py
@@ -9,6 +9,5 @@ class LBSteveDatabaseCheck(AgentCheck):
     def check(self, instance):
         r = requests.post("http://localhost:55183/metrics", headers={'Content-Type': 'application/json'}, data=json.dumps({}))
         metrics = json.loads(r.text)
-        self.gauge('lb.steve.running', metrics['running'])
-        self.gauge('lb.steve.queued', metrics['queued'])
-        self.gauge('lb.steve.total', metrics['total'])
+        for metric in metrics['metrics']:
+            self.gauge(metric['key'], metric['int_value'])
