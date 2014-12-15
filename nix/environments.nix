bc28e436c199b4bbd41ee7905e7dd77e54bba859
Rob Vermaas <rob.vermaas@logicblox.com>
2014-12-15 10:24:53 +0000

    Add all prod queues to test.

--- nix/environments.nix
+++ nix/environments.nix
@@ -8,7 +8,7 @@
   test =
     { hostName = "steve-test.logicblox.com";
       elasticIPv4 = "23.21.124.192";
-      workers = { "c3.xlarge" = { number = 1; price = "0.25"; }; };
+      workers = { "c3.xlarge" = { number = 1; price = "0.25"; }; "r3.xlarge" = { number = 0; price = "0.40"; }; "r3.2xlarge" = { number = 0; price = "0.75"; }; };
     };
 
   thiago =
