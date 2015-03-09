f06a3671f8dedb3415df88214a0260874fdd5c42
Rob Vermaas <rob.vermaas@logicblox.com>
2015-03-09 17:35:00 +0000

    Update image. Timeout i2-xlarge -> i2-2xlarge

--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -20,7 +20,7 @@ public class Main {
   private static String queue = "c3-xlarge";
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-2eecba46";
+  private static String ami = "ami-caf2d6a2";
   private static String key = "rob";
   private static String s3Bucket = "steve-jobs";
   private static String instanceType = "c3.xlarge";
