b53a6623ea565a8c0fb4f2f313648fec190735d1
Rob Vermaas <rob.vermaas@logicblox.com>
2015-03-13 10:42:10 +0000

    Simplify image creation script

--- create-s3-amis.sh
+++ create-s3-amis.sh
@@ -6,34 +6,28 @@ else
   p="cat $1"
 fi
 
-export NIXOS_CONFIG=$(dirname $(readlink -f $0))/nix/worker-ec2-image.nix
 export TIMESTAMP=$(date +%Y%m%d%H%M)
 
-    system="x86_64-linux"
-    arch="x86_64"
+arch="x86_64"
 
-    echo "downloading $system image..."
-    rm -f /tmp/nixos.img.*
-    $p | xz -d > /tmp/nixos.img
+echo "downloading image..."
+rm -f /tmp/nixos.img.*
+$p | xz -d > /tmp/nixos.img
 
-    ec2-bundle-image -i /tmp/nixos.img --user "$AWS_ACCOUNT" --arch "$arch" \
-        -c "$EC2_CERT" -k "$EC2_PRIVATE_KEY"
+ec2-bundle-image -i /tmp/nixos.img --user "$AWS_ACCOUNT" --arch "$arch" \
+    -c "$EC2_CERT" -k "$EC2_PRIVATE_KEY"
 
-    for region in us-east-1; do
-        echo "uploading $system image for $region..."
+echo "uploading image..."
 
-        name=steve-jobs-worker-$arch-s3
-        bucket="steve-jobs-worker"
+name=steve-jobs-worker-$arch-s3
+bucket="steve-jobs-worker"
 
-        if [ "$region" = eu-west-1 ]; then s3location=EU;
-        elif [ "$region" = us-east-1 ]; then s3location=US;
-        else s3location="$region"
-        fi
-
-        ec2-upload-bundle --retry -b "$bucket/$TIMESTAMP" -m /tmp/nixos.img.manifest.xml \
-            -a "$EC2_ACCESS_KEY" -s "$EC2_SECRET_KEY" --location "$s3location" \
+ec2-upload-bundle --retry -b "$bucket/$TIMESTAMP" -m /tmp/nixos.img.manifest.xml \
+            -a "$EC2_ACCESS_KEY" -s "$EC2_SECRET_KEY" --location US \
             --url http://s3.amazonaws.com
 
-        aws ec2 register-image --image-location $bucket/$TIMESTAMP/nixos.img.manifest.xml --name "$name $TIMESTAMP $(basename $p)" --region us-east-1 --virtualization-type hvm
-    done
+aws ec2 register-image --image-location $bucket/$TIMESTAMP/nixos.img.manifest.xml --name "$name $TIMESTAMP $(basename $p)" --region us-east-1 --virtualization-type hvm | tee image.json
+
+sed -i "s|\"ami-.*\"|\"$(cat image.json | json ImageId)\"|" worker/java/com/logicblox/steve/provision/Main.java
+
 
