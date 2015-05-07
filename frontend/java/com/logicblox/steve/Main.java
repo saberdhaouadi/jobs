ccc5036610e1db002a62ec9854ecb014d79da373
Rob Vermaas <rob.vermaas@logicblox.com>
2015-05-07 09:16:13 +0000

    Backout 574:d5bb5286efc9, 'fixing call to service map scanner after it changed its api'

--- frontend/java/com/logicblox/steve/Main.java
+++ frontend/java/com/logicblox/steve/Main.java
@@ -91,7 +91,7 @@ public class Main {
 
   private void loadServiceContext() throws Exception {
     try {
-      _ctx.getServiceMapScanner().rescan(_ctx, Collections.singletonList("steve"), true, false);
+      _ctx.getServiceMapScanner().rescan(_ctx, Collections.singletonList("steve"), true);
     } catch (Error e) {
       throw e;
     } catch (Exception e) {
