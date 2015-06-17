d4ef4f11266457feccb862f6970dafae03d06c69
Rob Vermaas <rob.vermaas@logicblox.com>
2015-06-17 11:23:55 +0000

    Allow configuration of percentage-queue and max per queue for provisioners.

--- nix/environments.nix
+++ nix/environments.nix
@@ -8,7 +8,7 @@ rec {
         "r3.xlarge" = { number = 0; price = "0.40"; percentageSpot = "1.0"; };
         "r3.2xlarge" = { number = 0; price = "0.75"; percentageSpot = "0.9"; };
         "r3.8xlarge" = { number = 0; price = "3.00"; percentageSpot = "1.0"; };
-        "i2.2xlarge" = { number = 0; price = "3.00"; percentageSpot = "0"; };
+        "i2.2xlarge" = { number = 0; price = "3.00"; percentageSpot = "0"; percentageQueue = "1.0"; max = "200"; };
       };
     };
 
