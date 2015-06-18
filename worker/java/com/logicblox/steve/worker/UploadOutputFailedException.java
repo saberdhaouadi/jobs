18eb062e329d50d50f209abd4f2d7389cc1f0400
Rob Vermaas <rob.vermaas@logicblox.com>
2015-06-18 08:33:55 +0000

    Missing file

---
+++ worker/java/com/logicblox/steve/worker/UploadOutputFailedException.java
@@ -0,0 +1,7 @@
+package com.logicblox.steve.worker;
+
+public class UploadOutputFailedException extends Exception {
+  public UploadOutputFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
