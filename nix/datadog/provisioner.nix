7768381d7312a5a0b8fd87aaf1c650ac9470d2ce
Rob Vermaas <rob.vermaas@logicblox.com>
2015-07-01 09:50:03 +0000

    Add billing estimated charges metric

--- nix/datadog/provisioner.nix
+++ nix/datadog/provisioner.nix
@@ -1,22 +1,25 @@
 { config, pkgs, lib, ... }:
-{
-  systemd.services.dd-agent.environment.PYTHONPATH = "${pkgs.pythonPackages.boto}/lib/python2.7/site-packages";
-  environment.etc =
-    let
-      lb-jobs-provisioner-config =
-        pkgs.writeText "lb-jobs-provisioner.yaml" ''
-          init_config:
+let
+  generic-config =
+    pkgs.writeText "generic.yaml" ''
+      init_config:
 
-          instances:
-            [{}]
-        '';
-    in [
-      { source = lb-jobs-provisioner-config;
-        target = "dd-agent/conf.d/lb-jobs-provisioner.yaml";
-      }
-      { source = ./lb-jobs-provisioner.py;
-        target = "dd-agent/checks.d/lb-jobs-provisioner.py";
-      }
-    ];
+      instances:
+        [{}]
+    '';
+  billing = import <global_creds/billing.nix>;
+in
+{
+  systemd.services.dd-agent.environment = {
+    PYTHONPATH = "${pkgs.pythonPackages.boto}/lib/python2.7/site-packages";
+    AWS_BILLING_ACCESS_KEY = billing.access;
+    AWS_BILLING_SECRET_KEY = billing.secret;
+  };
+
+  environment.etc."dd-agent/conf.d/lb-jobs-provisioner.yaml".source = generic-config;
+  environment.etc."dd-agent/checks.d/lb-jobs-provisioner.py".source = ./lb-jobs-provisioner.py;
+
+  environment.etc."dd-agent/conf.d/cloudwatch-billing.yaml".source = generic-config;
+  environment.etc."dd-agent/checks.d/cloudwatch-billing.py".source = ./cloudwatch-billing.py;
 }
 
