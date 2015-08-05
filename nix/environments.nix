ff96942081cc361ff1d8fd3b316731988a36ff5d
Rob Vermaas <rob.vermaas@logicblox.com>
2015-08-05 13:59:29 +0000

    Add hi1-4xlarge queue

--- nix/environments.nix
+++ nix/environments.nix
@@ -6,6 +6,7 @@ rec {
         "c3.xlarge" = { number = 0; price = "0.25"; percentageSpot = "0.9"; };
         "c3.xlarge-online" = { number = 2; price = "0.25"; percentageSpot = "0"; instanceType = "c3.xlarge"; };
         "c3.4xlarge" = { number = 0; price = "0.84"; percentageSpot = "1.0"; };
+        "hi1.4xlarge" = { number = 0; price = "3.3"; percentageSpot = "1.0"; };
         "r3.xlarge" = { number = 0; price = "0.40"; percentageSpot = "1.0"; };
         "r3.2xlarge" = { number = 0; price = "0.75"; percentageSpot = "0.9"; };
         "r3.8xlarge" = { number = 0; price = "3.00"; percentageSpot = "1.0"; };
