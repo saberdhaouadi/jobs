1a89d845bb56a2e472ab5636c8e8b0c99d9e7c66
Rob Vermaas <rob.vermaas@logicblox.com>
2015-08-11 13:57:28 +0000

    Update image. Make threshold higher to start more workers when few jbos are in queue. Will be a bit more expensive, but nicer for users, and costs should not make a big difference over a full month.

--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -20,7 +20,7 @@ public class Main {
   private static String queue = "c3-xlarge";
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-bfdd0bd4";
+  private static String ami = "ami-bfce66d4";
   private static String key = "rob";
   private static String region = "us-east-1";
   private static String s3Bucket = "steve-jobs";
@@ -219,7 +219,7 @@ public class Main {
     // For workloads where only a few jobs are queued/running, start instance for each.
     // This will prevent the most common scenario, where we get notified by jobs that are
     // queued longer than an hour.
-    if (busyMsgs + waitingMsgs <= 5) {
+    if (busyMsgs + waitingMsgs <= 15) {
       minInstances = busyMsgs + waitingMsgs;
     }
 
