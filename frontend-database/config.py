ccf218e3d498b193273aadc9ce6d77711a2d51d2
Rob Vermaas <rob.vermaas@logicblox.com>
2015-08-17 11:47:17 +0000

    Make config.py compatible with 4.1.7 and newer LB version.

--- frontend-database/config.py
+++ frontend-database/config.py
@@ -1,4 +1,5 @@
 from lbconfig.api import *
+import lbconfig.core
 
 protocols_dep = ("protocols", {'default_path': "/opt/logicblox/lb-steve-protocols"})
 
@@ -39,5 +40,10 @@ check_lb_workspace(
 
 check_program('tests/basic.py', ['lb-steve-frontend-database-test'])
 
-install_dir('$(protocols)/share/lb_steve_protocols', 'share/lb_steve_protocols')
+import subprocess
+if subprocess.check_output(["lb", "version"]).strip() != "4.1.7":
+    install_dir('$(protocols)/share/lb-steve-protocols/lb_steve_protocols', 'share/lb-steve-protocols/lb_steve_protocols')
+else:
+    install_dir('$(protocols)/share/lb_steve_protocols', 'share/lb_steve_protocols')
+
 install_dir('workflows', 'workflows')
