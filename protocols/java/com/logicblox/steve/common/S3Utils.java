d4fd4b9061c16d7d0625a263d98cb436504cb1f9
Rob Vermaas <rob.vermaas@logicblox.com>
2015-08-30 15:05:09 +0000

    Set retry count for s3 to 7 in stead of 3.

--- protocols/java/com/logicblox/steve/common/S3Utils.java
+++ protocols/java/com/logicblox/steve/common/S3Utils.java
@@ -43,7 +43,7 @@ public class S3Utils {
     long chunkSize = Utils.getDefaultChunkSize();
 
     // TODO make retry count configurable
-    int retryCount = 3;
+    int retryCount = 7;
 
     S3Client result = new S3Client(
             (AWSCredentialsProvider)null,
