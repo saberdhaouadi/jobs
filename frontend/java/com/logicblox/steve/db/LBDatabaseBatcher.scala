2e048b7e5c430f22a7974eaa2a74b5b24a1a2643
Rob Vermaas <rob.vermaas@logicblox.com>
2015-10-08 11:29:01 +0000

    Use small batcher size for readonly queries.

--- frontend/java/com/logicblox/steve/db/LBDatabaseBatcher.scala
+++ frontend/java/com/logicblox/steve/db/LBDatabaseBatcher.scala
@@ -42,7 +42,7 @@ class LBDatabaseBatcher(client: ProtobufServiceClient, readOnly: Boolean) extend
   /**
    * Do not impose a limit in the number of requests per batch.
    */
-  override def maxSize = Int.MaxValue
+  override def maxSize = if (readOnly) 10 else Int.MaxValue
 
   /**
    * Use different window for readOnly batcher.
