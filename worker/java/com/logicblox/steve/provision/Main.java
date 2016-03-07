3a5e22f3e3e0ce5ae8ada7bd0dc9455e42f018eb
Rob Vermaas <rob.vermaas@logicblox.com>
2016-03-07 13:02:03 +0000

    Update worker ami

--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -20,7 +20,7 @@ public class Main {
   private static String queue = "c3-xlarge";
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-fd340397";
+  private static String ami = "ami-3e685054";
   private static String key = "rob";
   private static String region = "us-east-1";
   private static String s3Bucket = "steve-jobs";
