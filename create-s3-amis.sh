619aa39df246b0546edc2da2b443c7b84f2a66b5
Rob Vermaas <rob.vermaas@logicblox.com>
2016-04-27 08:09:15 +0000

    Update image and image script

--- create-s3-amis.sh
+++ create-s3-amis.sh
@@ -1,18 +1,15 @@
 #! /bin/sh -e
+build=0
+curl -o build.json -H 'Content-Type: application/json' -L -s https://bob.logicblox.com/job/jobs/default/worker_image.ec2/latest
 
-if [[ -z "$1" ]]; then
-  p="curl -L https://bob.logicblox.com/job/jobs/default/worker_image.ec2/latest/download-by-type/file/img"
-else
-  p="cat $1"
-fi
-
+build=$(cat build.json | json id)
 export TIMESTAMP=$(date +%Y%m%d%H%M)
 
 arch="x86_64"
 
 echo "downloading image..."
 rm -f /tmp/nixos.img.*
-$p | xz -d > /tmp/nixos.img
+curl -L https://bob.logicblox.com/build/$build/download-by-type/file/img | xz -d > /tmp/nixos.img
 
 ec2-bundle-image -i /tmp/nixos.img --user "$AWS_ACCOUNT" --arch "$arch" \
     -c "$EC2_CERT" -k "$EC2_PRIVATE_KEY"
@@ -22,11 +19,11 @@ echo "uploading image..."
 name=steve-jobs-worker-$arch-s3
 bucket="steve-jobs-worker"
 
-ec2-upload-bundle --retry -b "$bucket/$TIMESTAMP" -m /tmp/nixos.img.manifest.xml \
+ec2-upload-bundle --retry -b "$bucket/$TIMESTAMP-$build" -m /tmp/nixos.img.manifest.xml \
             -a "$EC2_ACCESS_KEY" -s "$EC2_SECRET_KEY" --location US \
             --url http://s3.amazonaws.com
 
-aws ec2 register-image --image-location $bucket/$TIMESTAMP/nixos.img.manifest.xml --name "$name $TIMESTAMP $(basename $p)" --region us-east-1 --virtualization-type hvm | tee image.json
+aws ec2 register-image --image-location $bucket/$TIMESTAMP-$build/nixos.img.manifest.xml --name "$name $TIMESTAMP build $build" --region us-east-1 --virtualization-type hvm | tee image.json
 
 sed -i "s|\"ami-.*\"|\"$(cat image.json | json ImageId)\"|" worker/java/com/logicblox/steve/provision/Main.java
 
--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -20,7 +20,7 @@ public class Main {
   private static String queue = "c3-xlarge";
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-74776d1e";
+  private static String ami = "ami-14d63379";
   private static String key = "rob";
   private static String region = "us-east-1";
   private static String s3Bucket = "steve-jobs";
