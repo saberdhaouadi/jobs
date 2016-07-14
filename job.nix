c7bc2c65eea85634f80b713b067d37b2ad871a37
Rob Vermaas <rob.vermaas@logicblox.com>
2016-07-14 08:14:37 +0000

    Serve the fancy report via hydra.

--- job.nix
+++ job.nix
@@ -235,16 +235,12 @@ let
         wssize=$(du -BM --max-depth=0 "$dudir" | sed 's/M//' | awk '{print $1}')
         echo "disk-usage-final,$wssize" >> $out/report/stats.csv
 
-        cp iousg-monitor.csv cpuusg-monitor.csv memusg-monitor.csv $out/report
-
-        iousg-monitor-plot  iousg-monitor.csv  $out/report/iousg  "lb-server"
-        cpuusg-monitor-plot cpuusg-monitor.csv $out/report/cpuusg "lb-server"
-        memusg-monitor-plot memusg-monitor.csv $out/report/memusg "lb-server"
-
         mkdir -p $out/nix-support
-        for f in $out/report/*; do
-          echo "file data $f" >> $out/nix-support/hydra-build-products
-        done
+        echo "file data $out/report/stats.csv" >> $out/nix-support/hydra-build-products
+
+        tar -C $out/report -xvzf $out/report/load-report.tar.gz
+        mv $out/report/report $out/report/load-report
+        echo "doc load-report $out/report/load-report"
       '';
     };
   });
