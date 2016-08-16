c08cef98d3f73348bd61a57015577226608944e7
Rob Vermaas <rob.vermaas@logicblox.com>
2016-08-16 18:55:26 +0000

    Add i2-4xlarge for testing.

--- nix/environments.nix
+++ nix/environments.nix
@@ -8,10 +8,11 @@ rec {
         "c3.4xlarge" = { number = 0; price = "0.84"; percentageSpot = "1.0"; min = "75"; };
         "hi1.4xlarge" = { number = 0; price = "3.3"; percentageSpot = "1.0"; max = "100"; percentageQueue = "1.0"; min = "50"; };
         "r3.xlarge" = { number = 0; price = "0.40"; percentageSpot = "1.0"; max = "500"; };
-        "r3.2xlarge" = { number = 0; price = "0.75"; percentageSpot = "1.0"; };
+        "r3.2xlarge" = { number = 0; price = "0.75"; percentageSpot = "1.0"; max = "500"; };
         "r3.8xlarge" = { number = 0; price = "3.00"; percentageSpot = "1.0"; percentageQueue = "1.0"; min = "50"; };
         "i2.xlarge" = { number = 0; price = "0.86"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "200"; min = "75"; };
         "i2.2xlarge" = { number = 0; price = "1.71"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "300"; min = "75"; };
+        "i2.4xlarge" = { number = 0; price = "3.42"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "100"; min = "75"; };
       };
     };
 
