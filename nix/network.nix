65847c393b7cb5317f1055d8c8006ea38caff8ec
Rob Vermaas <rob.vermaas@logicblox.com>
2016-08-23 09:15:29 +0000

    Whitelist IP

--- nix/network.nix
+++ nix/network.nix
@@ -390,7 +390,7 @@ with pkgs.lib;
         "54.245.251.243" # wag-us-west
         "54.214.251.229" # wag-us-west
         "52.208.177.170" # kiabi-fred-catchup
-
+        "54.82.66.104"
       ];
       accountEntry = account:
         {
