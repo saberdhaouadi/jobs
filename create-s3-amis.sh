32829b2961f26af66af5e325ca4eeffaefbcd387
Rob Vermaas <rob.vermaas@logicblox.com>
2016-11-08 10:29:13 +0000

    Add copy image call to S3 image creation, save to nix file.

--- create-s3-amis.sh
+++ create-s3-amis.sh
@@ -30,6 +30,14 @@ ec2-upload-bundle --retry -b "$bucket/$TIMESTAMP-$build" -m /tmp/nixos.img.manif
 
 aws ec2 register-image --image-location $bucket/$TIMESTAMP-$build/nixos.img.manifest.xml --name "$name $TIMESTAMP build $build" --region us-east-1 --virtualization-type hvm | tee image.json
 
+ami=$(cat image.json | json ImageId)
 sed -i "s|\"ami-.*\"|\"$(cat image.json | json ImageId)\"|" worker/java/com/logicblox/steve/provision/Main.java
 
+echo "{" > nix/amis.nix
+echo "  us-east-1 = \"$ami\"" >> nix/amis.nix
+for region in us-west-1 us-west-2; do
+  aws ec2 copy-image --region $region --source-region us-east-1 --source-image-id $ami --name "$name $TIMESTAMP build $build" | tee $region.json
+  echo "  $region = \"$(cat $region.json | json ImageId)\"" >> nix/amis.nix
+done
 
+echo "}" >> nix/amis.nix
