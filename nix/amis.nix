94cb9f2d80cb96ace804c50bb4c349f8bcd53201
Rob Vermaas <rob.vermaas@logicblox.com>
2016-12-07 14:52:44 +0000

    Fix typos in amis.nix

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-46c5e851"
-  us-west-1 = "ami-ec0f5a8c"
-  us-west-2 = "ami-c7993aa7"
+  us-east-1 = "ami-46c5e851";
+  us-west-1 = "ami-ec0f5a8c";
+  us-west-2 = "ami-c7993aa7";
 }
