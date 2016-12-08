956f4bb62aaf5ab30497bf6a2fdf214c9209246c
Rob Vermaas <rob.vermaas@logicblox.com>
2016-12-08 10:18:48 +0000

    Another fix for amis.nix generation.

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-46c5e851";
-  us-west-1 = "ami-ec0f5a8c";
-  us-west-2 = "ami-c7993aa7";
+  us-east-1 = "ami-8ec0c199";
+  us-west-1 = "ami-4af6a12a";
+  us-west-2 = "ami-d4b912b4";
 }
