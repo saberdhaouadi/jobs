ec834c107e9eb3540d47f44ac40f62cd82fbb510
George Kollias <george.kollias@logicblox.com>
2017-03-10 18:50:05 +0200

    Another attempt

--- job.nix
+++ job.nix
@@ -92,7 +92,7 @@ let
 
           echo "command: $@"
           local t1="$(date +%s.%N)"
-          $@
+          "$@" || true
           local t2="$(date +%s.%N)"
 
           if ! type -P bc &> /dev/null; then
@@ -363,7 +363,7 @@ let
 */
     benchmark.dev-walgreens-jobs-run-1h =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "${jobs.database.build}/install.sh" ''
-        record_span "lb-walgreens-jobs-1h" "timeout -k 60 10m mitmdump -nc ${requests_dev_20170303-101311} | tee tmp.log || true"
+        record_span "lb-walgreens-jobs-1h" timeout -k 60 10m mitmdump -nc ${requests_dev_20170303-101311}
       '' "metrics" { buildInputs = [ mitmproxy ]; };
   });
 
