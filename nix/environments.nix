d2698bed4ced8097d559a6a35b9f02cc1c9f2b62
Rob Vermaas <rob.vermaas@logicblox.com>
2017-03-13 10:47:45 +0000

    Set max for c3-2xlarge to 500 in stead of 100

--- nix/environments.nix
+++ nix/environments.nix
@@ -15,7 +15,7 @@ rec {
       workers = addOnDemandQueues {
         "c3.xlarge" = { number = 0; price = "0.25"; percentageSpot = "1.0"; onDemand = true; };
         "c3.xlarge-online" = { number = 1; price = "0.25"; percentageSpot = "0"; instanceType = "c3.xlarge"; };
-        "c3.2xlarge" = { number = 0; price = "0.25"; percentageSpot = "1.0"; max = "100"; };
+        "c3.2xlarge" = { number = 0; price = "0.25"; percentageSpot = "1.0"; max = "500"; };
         "c3.4xlarge" = { number = 0; price = "0.84"; percentageSpot = "1.0"; percentageQueue = "1.0"; min = "75"; };
         "hi1.4xlarge" = { number = 0; price = "3.3"; percentageSpot = "1.0"; max = "100"; percentageQueue = "1.0"; min = "50"; };
         "r3.xlarge" = { number = 0; price = "0.40"; percentageSpot = "1.0"; max = "500"; };
