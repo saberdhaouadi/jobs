cdcdcd5f643eb6e5a69c4063b8c3f5030ba31753
Rob Vermaas <rob.vermaas@logicblox.com>
2017-03-23 16:36:30 +0000

    Fix /etc/hosts on ec2 image to allow rsyslog to pick it up properly

--- nix/worker-ec2-image.nix
+++ nix/worker-ec2-image.nix
@@ -22,7 +22,7 @@
         hostname=$(wget -q --retry-connrefused -t 6 --waitretry=10 -O -  http://169.254.169.254/latest/meta-data/instance-id)
         hostname $hostname
         cat > /tmp/hosts <<EOF
-        127.0.0.1 localhost $hostname
+        127.0.0.1 $hostname localhost
         ::1 localhost
         EOF
         mv /tmp/hosts /etc/hosts
