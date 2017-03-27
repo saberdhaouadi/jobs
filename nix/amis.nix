5649ff195e047113922686ef33679ed01552f190
Rob Vermaas <rob.vermaas@logicblox.com>
2017-03-27 08:35:37 +0000

    New images

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-dfec32c9";
-  us-west-1 = "ami-dc89d7bc";
-  us-west-2 = "ami-62a02202";
+  us-east-1 = "ami-66338c70";
+  us-west-1 = "ami-ef96cd8f";
+  us-west-2 = "ami-03b42163";
 }
