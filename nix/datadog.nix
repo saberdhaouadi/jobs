ab1953f01ab8df09101c6d1809adec59801618af
Rob Vermaas <rob.vermaas@logicblox.com>
2017-04-04 09:22:53 +0000

    Initial datadog timeboard.

---
+++ nix/datadog.nix
@@ -0,0 +1,133 @@
+{ name
+, ...
+}:
+let
+  inherit (import <nixpkgs> {}) lib;
+
+  env = (import ./environments.nix)."${name}";
+  queues = map (w: lib.replaceChars ["."] ["-"] w) (builtins.attrNames env.workers);
+
+  creds = {
+    appKey = builtins.readFile <global_creds/datadog-app-key>;
+    apiKey = builtins.readFile <global_creds/datadog-api-key>;
+  };
+
+  perQueueGraph = title: toMetric:
+    { inherit title;
+      definition = builtins.toJSON {
+        viz = "timeseries";
+        requests = map (q: { q = toMetric (dash-to-underscore q); type = "line"; }) queues;
+        autoscale = true;
+      };
+    };
+
+  dash-to-underscore = lib.replaceChars ["-"] ["_"];
+in
+{
+  resources.datadogTimeboards."lb-jobs-${name}" = 
+    { title = "LB Jobs (${name})";
+      description = "";
+      graphs = [
+        (perQueueGraph "Queued jobs" (q: "max:lb.steve.queued.${q}{host:database-${name}}"))
+        (perQueueGraph "Running jobs" (q: "max:lb.steve.running.${q}{host:database-${name}}"))
+        (perQueueGraph "Maximum queued time" (q: "max:lb.steve.queued_time.${q}.max{host:database-${name}}"))
+        (perQueueGraph "Queued jobs (hourly delta)" (q: "max:lb.steve.queued.${q}{host:database-${name}} - hour_before(max:lb.steve.queued.${q}{host:database-${name}})"))
+
+        { title = "Estimated Charges";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "avg:lb.steve.estimated_charges{*}";
+                type = "line";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+
+        { title = "Requests / s";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "max:nginx.net.request_per_s{host:steve-${name}}";
+                type = "line";
+              }
+              {
+                q = "max:lb.web.responses.total{host:database-${name}}.as_rate()";
+                type = "line";
+              }
+              {
+                q = "max:lb.web.responses.total{host:steve-${name}}.as_rate()";
+                type = "line";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+
+        { title = "Batcher sizes";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "avg:lb.steve.internal.batcher.read.size.avg{host:steve-${name}}";
+                type = "line";
+              }
+              {
+                q = "avg:lb.steve.internal.batcher.write.size.avg{host:steve-${name}}";
+                type = "line";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+
+        { title = "500 errors";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "avg:lb.web.responses.5xx{host:steve-${name}}.as_count()";
+                type = "bars";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+
+        { title = "Memory usage lb-server / system";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "avg:system.mem.usable{host:database-${name}}";
+                type = "line";
+              }
+              {
+                q = "avg:system.processes.mem.rss{host:database-${name},process_name:lb-server}";
+                type = "line";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+      ];
+    } // creds;
+}
+
+/*
+        { title = "";
+          definition = builtins.toJSON {
+            viz = "timeseries";
+            requests = [
+              {
+                q = "";
+                type = "line";
+              }
+            ];
+            autoscale = true;
+          };
+        }
+*/
