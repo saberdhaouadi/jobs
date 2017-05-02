292888303dbd0cd79ddd2443ee64199a7229f313
Rob Vermaas <rob.vermaas@logicblox.com>
2017-05-02 11:00:40 +0000

    Update image

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-66338c70";
-  us-west-1 = "ami-ef96cd8f";
-  us-west-2 = "ami-03b42163";
+  us-east-1 = "ami-47c3a151";
+  us-west-1 = "ami-91d2f5f1";
+  us-west-2 = "ami-adba22cd";
 }
--- worker/java/com/logicblox/steve/provision/Main.java
+++ worker/java/com/logicblox/steve/provision/Main.java
@@ -21,7 +21,7 @@ public class Main {
   private static String queue = "c3-xlarge";
   private static String incoming_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs";
   private static String outgoing_url = "https://sqs.us-east-1.amazonaws.com/297794765570/steve-jobs-results";
-  private static String ami = "ami-66338c70";
+  private static String ami = "ami-47c3a151";
   private static String key = "rob";
   private static String region = "us-east-1";
   private static String s3Bucket = "steve-jobs";
