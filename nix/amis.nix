b83760a79c1a302a91b2ff8ad3107e21f8ed81c1
Rob Vermaas <rob.vermaas@logicblox.com>
2017-05-26 11:11:31 +0000

    Update worker image

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-9586c683";
-  us-west-1 = "ami-e93d1d89";
-  us-west-2 = "ami-1285e572";
+  us-east-1 = "ami-22571a34";
+  us-west-1 = "ami-96fedef6";
+  us-west-2 = "ami-dd583bbd";
 }
