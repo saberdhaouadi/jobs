ea0df002c6c4a282df3c2089ca3d5c3132cb4c0b
Rob Vermaas <rob.vermaas@logicblox.com>
2017-05-31 08:17:07 +0000

    Enable Walgreens benchmark again to see if some memory leak is obvious.

--- job.nix
+++ job.nix
@@ -395,13 +395,11 @@ let
         tracing_json_publish "dev-1000-jobs-run"
       '' "metrics" { buildInputs = [ pkgs.pythonPackages.mitmproxy ]; };
 
-/*
     benchmark.dev-walgreens-jobs-run =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
         record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311}
       '' "metrics" { buildInputs = [ pkgs.pythonPackages.mitmproxy ]; };
-*/
   });
 
 in jobs
