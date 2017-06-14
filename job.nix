9afcf5a52ce7e5abf40021b3dd5f5b230dc02cfb
Rob Vermaas <rob.vermaas@logicblox.com>
2017-06-14 18:21:39 +0000

    Update to running an hour in stead of 30 minutes, and extend timeout for build to 3 hours.

--- job.nix
+++ job.nix
@@ -430,7 +430,7 @@ let
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
         record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311}
-      '' "metrics" { buildInputs = [ pkgs.pythonPackages.mitmproxy ]; timeout = 3600 ; meta.timeout = 7200; };
+      '' "metrics" { buildInputs = [ pkgs.pythonPackages.mitmproxy ]; timeout = 3600 ; meta.timeout = 10800; };
   });
 
 in jobs
