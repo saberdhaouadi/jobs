058a18996c2c4699ddb007ec9ef027bdc9e4d7ce
George Kollias <george.kollias@logicblox.com>
2017-06-19 02:23:02 +0300

    Let it fail

--- job.nix
+++ job.nix
@@ -95,7 +95,7 @@ let
           shift
 
           local t1="$(date +%s.%N)"
-          ${pkgs.lib.optionalString (attrs ? timeout) "timeout ${toString attrs.timeout}"} "$@" ${pkgs.lib.optionalString (attrs ? timeout) "|| true"}
+          ${pkgs.lib.optionalString (attrs ? timeout) "timeout ${toString attrs.timeout}"} "$@"
           local t2="$(date +%s.%N)"
 
           if ! type -P bc &> /dev/null; then
