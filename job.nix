7346985963bbdc20185ff9413b024f73a4761389
George Kollias <george.kollias@logicblox.com>
2017-06-19 20:48:11 +0300

    Trigger

--- job.nix
+++ job.nix
@@ -470,7 +470,7 @@ let
     benchmark.dev-walgreens-jobs-data2x-3h =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
-        record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311}
+        record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311} 
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 10800; dataset_multiplier = 2; meta.timeout = 36000; meta.maxSilent = 36000; };
 
     benchmark.dev-walgreens-jobs-run =
@@ -480,7 +480,7 @@ let
 
     benchmark.dev-walgreens-jobs-run-data2x-4h =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "${jobs.database.build}/install.sh" ''
-        record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311}
+        record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311} 
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 14400; dataset_multiplier = 2; meta.timeout = 36000; meta.maxSilent = 36000; };
 
     benchmark.dev-walgreens-jobs-install =
