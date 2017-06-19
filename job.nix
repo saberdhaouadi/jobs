0c135797aa26720ae6f48958839c3eac915823a2
George Kollias <george.kollias@logicblox.com>
2017-06-19 17:38:15 +0300

    Disable 32x & 64x data loads

--- job.nix
+++ job.nix
@@ -497,7 +497,7 @@ let
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 43200; dataset_multiplier = 8; meta.timeout = 43200; };
- 
+/* 
     benchmark.dev-walgreens-jobs-install-data32x =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
@@ -507,7 +507,7 @@ let
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 43200; dataset_multiplier = 64; meta.timeout = 43200; };
- 
+*/ 
   });
 
 in jobs
