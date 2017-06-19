ce359e46dc01b9a6154f2401575bbdf3faa1b6c3
George Kollias <george.kollias@logicblox.com>
2017-06-19 04:04:37 +0300

    2x scale

--- job.nix
+++ job.nix
@@ -476,6 +476,11 @@ let
         record_span "run-installer" ${jobs.database.build}/install.sh
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 7200; meta.timeout = 18000; };
  
+    benchmark.dev-walgreens-jobs-install-data2x =
+      bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
+        record_span "run-installer" ${jobs.database.build}/install.sh
+      '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 36000; dataset_multiplier = 2; meta.timeout = 36000; };
+
     benchmark.dev-walgreens-jobs-install-data4x =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
         record_span "run-installer" ${jobs.database.build}/install.sh
