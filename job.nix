e3103db0fdc7cdf5bd7945ad6b495f2995e1e132
George Kollias <george.kollias@logicblox.com>
2017-06-23 22:47:33 +0300

    15m

--- job.nix
+++ job.nix
@@ -526,11 +526,16 @@ let
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "${jobs.database.build}/install.sh" ''
         record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311} 
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 14400; dataset_multiplier = 2; meta.timeout = 36000; meta.maxSilent = 36000; };
-*/
+
     benchmark.dev-walgreens-jobs-run-data2x-12h =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "${jobs.database.build}/install.sh" ''
         record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311} 
       '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 12*60*60; dataset_multiplier = 2; meta.timeout = 20*60*60; meta.maxSilent = 20*60*60; };
+*/
+    benchmark.dev-walgreens-jobs-run-data2x-15m =
+      bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "${jobs.database.build}/install.sh" ''
+        record_span "lb-walgreens-jobs" mitmdump -nc ${requests_dev_20170303-101311} 
+      '' "metrics" { buildInputs = [ mitmproxy ]; timeout = 15*60; dataset_multiplier = 2; meta.timeout = 20*60*60; meta.maxSilent = 20*60*60; };
 /*
     benchmark.dev-walgreens-jobs-install =
       bench data_dev_20170303-101311 "lb-walgreens-jobs-run" "" ''
