882db625884d1fd8320ac747ac1b3bd04eecbdc2
Rob Vermaas <rob.vermaas@logicblox.com>
2017-06-29 14:36:56 +0000

    Fix memleak job

--- sample-jobs/memleak/a.c
+++ sample-jobs/memleak/a.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
         while(1)
         {
                 void *m = malloc(1024*1024);
+                if (!m) break;
                 memset(m,0,1024*1024);
         }
         return 0;
