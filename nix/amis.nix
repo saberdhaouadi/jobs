1629f0121735c2755f9f2a5b05baef4fa1328e5e
Rob Vermaas <rob.vermaas@logicblox.com>
2017-07-06 20:04:55 +0000

    Update image

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-9f5f6489";
-  us-west-1 = "ami-32240b52";
-  us-west-2 = "ami-63f9e91a";
+  us-east-1 = "ami-b56c68a3";
+  us-west-1 = "ami-f8f8d798";
+  us-west-2 = "ami-90c8dbe9";
 }
