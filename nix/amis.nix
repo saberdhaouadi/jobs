fca6d89d1252f8b9cfc3c2299e8ff31a55eff7f1
Rob Vermaas <rob.vermaas@logicblox.com>
2017-07-20 12:08:50 +0000

    New images

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,5 +1,5 @@
 {
-  us-east-1 = "ami-9f5f6489";
-  us-west-1 = "ami-32240b52";
-  us-west-2 = "ami-63f9e91a";
+  us-east-1 = "ami-846e6992";
+  us-west-1 = "ami-44c2ed24";
+  us-west-2 = "ami-d3b7a4aa";
 }
