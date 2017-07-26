73bf4d851c9610a6f5817fc6db0b7fa75bdd5aa9
George Kollias <george.kollias@logicblox.com>
2017-07-27 00:39:42 +0300

    Build platform from source

--- default.nix
+++ default.nix
@@ -1,13 +1,30 @@
-{ platform_release ? import ./lb-version.nix
+# { platform_release ? import ./lb-version.nix
+{ src_logicblox
+, src_lb_web_original
+, src_s3lib
+, src_datalog_generator
 , benchmarks ? null
 , heap_profiling ? false
 , nixpkgs_1703 ? null
 , bench_duration ? 30*60
+, extraDefines ? []
+, logicblox_config ? "release"
 }:
 let
   builder_config = import <config> {};
   inherit (builder_config) pkgs getLB;
-  platform = getLB platform_release;
+
+  integration_jobset =
+    import (src_builder_config + "/integration/jobsets.nix") {
+      inherit src_logicblox src_lb_web_original src_s3lib src_datalog_generator;
+      args_logicblox = {
+        inherit extraDefines;
+        config = logicblox_config;
+      };
+
+  platform = integration_jobset.platform;
+
+  # platform = getLB platform_release;
 in
   import ./job.nix {
     logicblox = platform;
