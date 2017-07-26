d77d3f821160a513996ca795427676aa30f54c9f
Wes Hunter <wes.hunter@logicblox.com>
2017-07-26 16:00:17 -0400

    updated jars to match new cloud-store requirements

--- deps.nix
+++ deps.nix
@@ -14,10 +14,10 @@ let
 
   aws-java-sdk =
     with pkgs; stdenv.mkDerivation rec {
-      name = "aws-java-sdk-1.10.37";
+      name = "aws-java-sdk-1.11.102";
       src = fetchurl {
-        url = http://sdk-for-java.amazonwebservices.com/aws-java-sdk-1.10.37.zip;
-        sha256 = "15gjq7zrv26cna9qniqv90hl6xdpw61c2lsk0jnhx62c7q1cr6kf";
+        url = http://sdk-for-java.amazonwebservices.com/aws-java-sdk-1.11.102.zip;
+        sha256 = "c06a529b86c08d73b840adc6fe103d49d7ff3eea011977267f0f350a333c2fb3";
       };
       buildInputs = [ pkgs.unzip ];
       buildCommand = ''
