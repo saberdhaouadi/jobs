19d923626e1e75df69644d5024a33479640cf975
Rob Vermaas <rob.vermaas@logicblox.com>
2017-09-11 12:46:06 +0000

    oops

--- job.nix
+++ job.nix
@@ -266,7 +266,7 @@ let
 
   jobs = rec {
 
-  tests = ./tests/lb-jobs-network.nix { builds = jobs; };
+  tests = import ./tests/lb-jobs-network.nix { builds = jobs; };
 
   frontend =
      builder_config.buildLBConfig {
