c249c4848f8cda79819539daa18f51f75b79a8e3
Amine Chikhaoui <amine.chikhaoui@predictix.com>
2017-11-27 11:34:04 -0500

    update amis/provisioner with kernel version 4.4

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-6a2abc10";
-  us-west-1.ebs = "ami-684c7708";
-  us-west-2.ebs = "ami-b8f629c0";
-  us-east-1.s3 = "ami-2c2bbd56";
-  us-west-1.s3 = "ami-644c7704";
-  us-west-2.s3 = "ami-d6f02fae";
+  us-east-1.ebs = "ami-8773e0fd";
+  us-west-1.ebs = "ami-1d0d367d";
+  us-west-2.ebs = "ami-b7dc02cf";
+  us-east-1.s3 = "ami-5b71e221";
+  us-west-1.s3 = "ami-7a0c371a";
+  us-west-2.s3 = "ami-1dd90765";
 }
