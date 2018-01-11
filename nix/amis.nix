0ece4381374c923ec579e6ea80dbd178edc6702e
Amine Chikhaoui <amine.chikhaoui@predictix.com>
2018-01-11 09:16:50 -0500

    update amis with lb services startup fix.

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-8773e0fd";
-  us-west-1.ebs = "ami-1d0d367d";
-  us-west-2.ebs = "ami-b7dc02cf";
-  us-east-1.s3 = "ami-5b71e221";
-  us-west-1.s3 = "ami-7a0c371a";
-  us-west-2.s3 = "ami-1dd90765";
+  us-east-1.ebs = "ami-32613b48";
+  us-west-1.ebs = "ami-a62e2dc6";
+  us-west-2.ebs = "ami-3503b44d";
+  us-east-1.s3 = "ami-76643e0c";
+  us-west-1.s3 = "ami-e8282b88";
+  us-west-2.s3 = "ami-a904b3d1";
 }
