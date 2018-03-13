11c38a8aa013d3d3651fb5139051792205858e1d
George Kollias <george.kollias@logicblox.com>
2018-03-13 15:52:05 +0200

    Build S3Client objects through S3ClientBuilder

--- protocols/java/com/logicblox/steve/common/S3Utils.java
+++ protocols/java/com/logicblox/steve/common/S3Utils.java
@@ -10,6 +10,7 @@ import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.logicblox.cloudstore.DirectoryKeyProvider;
 import com.logicblox.cloudstore.KeyProvider;
 import com.logicblox.cloudstore.S3Client;
+import com.logicblox.cloudstore.S3ClientBuilder;
 import com.logicblox.cloudstore.Utils;
 
 import com.logicblox.bloxweb.config.ConfigMap;
@@ -43,11 +44,11 @@ public class S3Utils {
     // TODO make retry count configurable
     int retryCount = 7;
 
-    S3Client result = new S3Client(
-            (AWSCredentialsProvider)null,
-            getHttpExecutor(config),
-            getInternalExecutor(config),
-            getKeyProvider(config));
+    S3Client result = new S3ClientBuilder()
+      .setApiExecutor(getHttpExecutor(config))
+      .setInternalExecutor(getInternalExecutor(config))
+      .setKeyProvider(getKeyProvider(config))
+      .createS3Client();
 
     result.setRetryCount(retryCount);
     if(config != null && config.isSome("s3_endpoint")) {
