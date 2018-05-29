7141179a4efd38958807d11382b46d37fb2da4d2
Chaker Benhamed <chaker.benhamed@predictix.com>
2018-05-29 11:53:10 +0100

    Restore old variable name.
    The current aws-sdk version in the nix-daemon in 17.09 is old. And it
    require this variable name.

--- nix/worker-gce-image.nix
+++ nix/worker-gce-image.nix
@@ -4,6 +4,7 @@ let
     {
       environment.AWS_ACCESS_KEY_ID = builtins.readFile <global_creds/gce-access>;
       environment.AWS_SECRET_ACCESS_KEY = builtins.readFile <global_creds/gce-secret>;
+      environment.AWS_SECRET_KEY = builtins.readFile <global_creds/gce-secret>;
       environment.AWS_REGION = "us-east-1";
       environment.GCS_XML_ACCESS_KEY = builtins.readFile <global_creds/gcs-access>;
       environment.GCS_XML_SECRET_KEY = builtins.readFile <global_creds/gcs-secret>;
