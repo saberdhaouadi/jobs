b965c3313e478193e7f6a1cf1de904a8443fe51c
Amine Chikhaoui <amine.chikhaoui@predictix.com>
2018-06-14 11:02:07 +0100

    remove tracing logs

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 let
   builder_config = import <config> {};
   inherit (builder_config) pkgs getLB;
-  platform = builtins.trace (builtins.typeOf platform_release) if (pkgs.lib.hasAttr "outPath") then platform_release.outPath else getLB platform_release;
+  platform = (if (pkgs.lib.hasAttr "outPath" platform_release) then platform_release.outPath else (getLB platform_release));
 in
   import ./job.nix {
     logicblox = platform;
