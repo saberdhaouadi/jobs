4d527f89f89a8f8f7a938ccfd50cec53384eb166
George Kollias <george.kollias@logicblox.com>
2018-06-26 19:10:28 +0300

    s:/opt/logicblox/cloudstore:/opt/logicblox/cloud-store:

--- worker/config.py
+++ worker/config.py
@@ -18,7 +18,7 @@ protocols_dep = (
   "protocols", {'default_path': "/opt/logicblox/lb-steve-protocols"}
 )
 
-aws_java_sdk_dep = ("aws_java_sdk", {'default_path': "/opt/logicblox/cloudstore"})
+aws_java_sdk_dep = ("aws_java_sdk", {'default_path': "/opt/logicblox/cloud-store"})
 
 depends_on(
     logicblox_dep,
