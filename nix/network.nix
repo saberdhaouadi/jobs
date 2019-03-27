f39366629dec835ab5af12eccd1ef2714376f0ee
AhmedSamti <devnull@localhost>
2019-03-27 06:43:49 -0400

    tagging the ressources in lb-jobs cluster

--- nix/network.nix
+++ nix/network.nix
@@ -150,6 +150,8 @@ with pkgs.lib;
 {
   network.description = "Steve Jobs [${name}]";
 
+  requires = [ <lbdevops/nixops/generic/tags.nix> ];
+
   resources.elasticIPs.key-ip-us-west-1 = { region = "us-west-1" ; accessKeyId = account; };
   "key-proxy-${name}-us-west-1" = key-proxy "us-west-1";
 
