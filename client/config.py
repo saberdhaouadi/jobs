eb8fd3b295bab1999b00ad0fcd58edb1c8539e77
Oussama Elkaceh <oussama.kaceh@gmail.com>
2019-04-23 17:17:26 -0400

    removing explicitly set Make target

--- client/config.py
+++ client/config.py
@@ -5,7 +5,7 @@ lbconfig_package(
   'lb-steve-client',
   version='1.0',
   default_prefix='/opt/logicblox/lb-steve-client',
-  default_targets=['jars', 'findbugs'])
+  default_targets=['jars'])
 
 protocols_dep = ("protocols", {'default_path': "/opt/logicblox/lb-steve-protocols"})
 aws_java_sdk_dep = ("aws_java_sdk", {'default_path': "/opt/logicblox/cloud-store"})
@@ -61,7 +61,6 @@ jar(
    srcdir = 'java',
    findbugs = True,
    classpath = classpath)
-core.g_rules['findbugs'].input = set()
 
 link_libs(classpath)
 
