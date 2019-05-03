c48cfd738e4e058e29b2ddb2bdb5e2ab5d491eb4
mhomrane <devnull@localhost>
2019-05-03 07:56:12 -0400

    new AMIs using build 4062763

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-032b49762ab0b5524";
-  us-west-1.ebs = "ami-0ea82795941393f75";
-  us-west-2.ebs = "ami-0e24092d6695de364";
-  us-east-1.s3 = "ami-04c74be58ef6ed328";
-  us-west-1.s3 = "ami-0c1bfed3f88cdf449";
-  us-west-2.s3 = "ami-016b7c48234f1e9ee";
+  us-east-1.ebs = "ami-04963cdf48c12a91d";
+  us-west-1.ebs = "ami-05cd85683953c7c90";
+  us-west-2.ebs = "ami-07e04daa5c8f907d1";
+  us-east-1.s3 = "ami-0b8234078ba2c590b";
+  us-west-1.s3 = "ami-079301bd7b3aee0ee";
+  us-west-2.s3 = "ami-0233ce577d96d0759";
 }
