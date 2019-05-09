5bd55ae25c7ff20d9adb36f8d2d4219d35177ce3
helmi nour <devnull@localhost>
2019-05-09 10:54:33 +0100

    LB-3573: fixing lb-steve-client build issue.

--- client/config.py
+++ client/config.py
@@ -5,7 +5,7 @@ lbconfig_package(
   'lb-steve-client',
   version='1.0',
   default_prefix='/opt/logicblox/lb-steve-client',
-  default_targets=['jars'])
+  default_targets=['jars', 'findbugs'])
 
 protocols_dep = ("protocols", {'default_path': "/opt/logicblox/lb-steve-protocols"})
 aws_java_sdk_dep = ("aws_java_sdk", {'default_path': "/opt/logicblox/cloud-store"})
