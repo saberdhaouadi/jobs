8381d3fab0e0633796eafcad2416e31010bafa4d
mhomrane <devnull@localhost>
2019-07-17 06:03:36 -0400

    generate EC2 worker AMIs with rpy2 fix

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-6dc1c717";
-  us-west-1.ebs = "ami-fb56599b";
-  us-west-2.ebs = "ami-681ea510";
-  us-east-1.s3 = "ami-42cacc38";
-  us-west-1.s3 = "ami-ee545b8e";
-  us-west-2.s3 = "ami-ab1ea5d3";
+  us-east-1.ebs = "ami-04963cdf48c12a91d";
+  us-west-1.ebs = "ami-05cd85683953c7c90";
+  us-west-2.ebs = "ami-07e04daa5c8f907d1";
+  us-east-1.s3 = "ami-0b8234078ba2c590b";
+  us-west-1.s3 = "ami-079301bd7b3aee0ee";
+  us-west-2.s3 = "ami-0233ce577d96d0759";
 }
