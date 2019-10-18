cecc2167e6b5f08ce381edff54c8829ac5479043
ridha.zorgui <devnull@localhost>
2019-10-18 09:07:29 -0400

    testing a worker config

--- nix/worker-ec2-image.nix
+++ nix/worker-ec2-image.nix
@@ -15,7 +15,7 @@
   networking.hostName = pkgs.lib.mkForce "i-worker";
 
   lb-steve-worker.shutdownOnIdle = true;
-
+  boot.initrd.kernelModules = [ "xen-blkfront" "xen-netfront" ];
   systemd.services.set-hostname =
     {
       before = [ "syslog.service" ];
@@ -36,9 +36,10 @@
           RemainAfterExit = true;
         };
     };
-
-  boot.kernelParams = lib.mkForce [ "boot.trace" "console=ttyS0" ];
-  boot.initrd.availableKernelModules = [ "nmve" ];
+  boot.blacklistedKernelModules = [ "nouveau" "xen_fbfront" ];
+  boot.kernelParams = [ "console=ttyS0" "boot.trace" ];
+  boot.initrd.availableKernelModules = [ "ixgbevf" "ena" "nvme" ];
+  
   boot.initrd.extraUtilsCommands =
     ''
       cp --remove-destination ${pkgs.e2fsprogs}/sbin/mke2fs $out/bin
@@ -66,51 +67,40 @@
       if ! [ -e "$metaDir/public-keys-0-openssh-key" ]; then
         wget -q -O "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
       fi
-
-      devices=""
-      nr=0
-      mkdir -p /var/lock/lvm
-      for device in /dev/xvd[bcdef]* /dev/nvme[0-9]n[0-9]; do
-        echo $device
-        if [ -e "$device" ]; then
-          lvm pvcreate -f $device
-          devices="$devices $device"
-          nr=$((nr+1))
-        fi
+      diskNr=0
+      diskForUnionfs=
+      for device in /dev/xvd[abcde]*; do
+          if [ "$device" = /dev/xvda -o "$device" = /dev/xvda1 ]; then continue; fi
+          fsType=$(blkid -o value -s TYPE "$device" || true)
+          if [ "$fsType" = swap ]; then
+              echo "activating swap device $device..."
+              swapon "$device" || true
+          elif [ "$fsType" = ext3 ]; then
+              mp="/disk$diskNr"
+              diskNr=$((diskNr + 1))
+              if mountFS "$device" "$mp" "" ext3; then
+                  if [ -z "$diskForUnionfs" ]; then diskForUnionfs="$mp"; fi
+              fi
+          else
+              echo "skipping unknown device type $device"
+          fi
       done
-
-      set -x
-      if [ -n "$devices" ]; then
-        echo "vgcreate"
-        lvm vgcreate raid $devices
-        echo "lvcreate"
-        lvm lvcreate -vvv --noudevsync --zero n raid --name raid --extents '100%FREE' --stripes $nr
-        echo "vgchange"
-        lvm vgchange --noudevsync -ay raid
-
-        diskForUnionfs=/disk0
-        echo "Creating ext4 filesystem on /dev/dm-0"
-        mke2fs -t ext4 /dev/dm-0
-        echo "Mounting /dev/dm-0 to $diskForUnionfs"
-        mountFS /dev/dm-0 $diskForUnionfs "" ext4
-
-        mkdir -m 755 -p $targetRoot/$diskForUnionfs/root
-        mkdir -m 1777 -p $targetRoot/$diskForUnionfs/root/tmp $targetRoot/tmp
-        mount --bind $targetRoot/$diskForUnionfs/root/tmp $targetRoot/tmp
-
-        mkdir -m 755 -p $targetRoot/$diskForUnionfs/root/var $targetRoot/var
-        mount --bind $targetRoot/$diskForUnionfs/root/var $targetRoot/var
-
-        mkdir -p /unionfs-chroot/ro-nix
-        mount --rbind $targetRoot/nix /unionfs-chroot/ro-nix
-
-        mkdir -m 755 -p $targetRoot/$diskForUnionfs/root/nix
-        mkdir -p /unionfs-chroot/rw-nix
-        mount --rbind $targetRoot/$diskForUnionfs/root/nix /unionfs-chroot/rw-nix
-
-        unionfs -o allow_other,cow,nonempty,chroot=/unionfs-chroot,max_files=32768 /rw-nix=RW:/ro-nix=RO $targetRoot/nix
+      if [ -n "$diskForUnionfs" ]; then
+          mkdir -m 755 -p $targetRoot/$diskForUnionfs/root
+          mkdir -m 1777 -p $targetRoot/$diskForUnionfs/root/tmp $targetRoot/tmp
+          mount --bind $targetRoot/$diskForUnionfs/root/tmp $targetRoot/tmp
+          if [ "$(cat "$metaDir/ami-manifest-path")" != "(unknown)" ]; then
+              mkdir -m 755 -p $targetRoot/$diskForUnionfs/root/var $targetRoot/var
+              mount --bind $targetRoot/$diskForUnionfs/root/var $targetRoot/var
+              mkdir -p /unionfs-chroot/ro-nix
+              mount --rbind $targetRoot/nix /unionfs-chroot/ro-nix
+              mkdir -m 755 -p $targetRoot/$diskForUnionfs/root/nix
+              mkdir -p /unionfs-chroot/rw-nix
+              mount --rbind $targetRoot/$diskForUnionfs/root/nix /unionfs-chroot/rw-nix
+              unionfs -o allow_other,cow,nonempty,chroot=/unionfs-chroot,max_files=32768 /rw-nix=RW:/ro-nix=RO $targetRoot/nix
+          fi
       fi
-      set +x
+     set +x
     '';
 
 
