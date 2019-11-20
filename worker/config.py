99bd2bd685b42e5047d9853a169d024c757d3b72
Amine Chikhaoui <amine.chikhaoui@predictix.com>
2019-11-20 15:28:47 -0500

    undo debug build

--- worker/config.py
+++ worker/config.py
@@ -116,7 +116,7 @@ provision_classpath = [
 jar(
    name = 'lb-steve-worker',
    srcdir = 'java/com/logicblox/steve/worker',
-   javac_flags = "-g",
+   #javac_flags = "-g",
    findbugs = True,
     classpath = worker_classpath + classpath)
 
