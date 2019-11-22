9cb86e82ba014c102de2ead490c2dc37341c1013
Amine Chikhaoui <amine.chikhaoui@predictix.com>
2019-11-22 10:37:05 -0500

    fix typo

--- worker/java/com/logicblox/steve/provision/AWSProvisioner.java
+++ worker/java/com/logicblox/steve/provision/AWSProvisioner.java
@@ -181,7 +181,7 @@ public class AWSProvisioner implements ProvisionerInterface {
 
     String getUserData() {
         return Base64.encodeBase64String(
-                String.format("WORKERARGS=\"--bucket %s --incoming %s --outgoing %s --key-service %s\"\nKEYSERVICE=\"%s\""",
+                String.format("WORKERARGS=\"--bucket %s --incoming %s --outgoing %s --key-service %s\"\nKEYSERVICE=\"%s\"",
                         cmdArgs.getS3Bucket(),
                         cmdArgs.getIncoming_url(),
                         cmdArgs.getOutgoing_url(),
