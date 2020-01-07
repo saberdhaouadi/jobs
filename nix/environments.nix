2ae4452d5d388fc9f8404e2d900f9f77d4f39698
AhmedSamti <devnull@localhost>
2020-01-07 09:07:15 -0500

    [27974] reverting old amis and overriding queues

--- nix/environments.nix
+++ nix/environments.nix
@@ -24,7 +24,7 @@ rec {
         "c3.4xlarge" = { number = 0; price = "0.84"; percentageSpot = "1.0"; percentageQueue = "1.0"; min = "75"; maxDelta = "100"; };
         "r3.xlarge" = { number = 0; price = "0.40"; percentageSpot = "1.0"; max = "500"; };
         "r3.2xlarge" = { number = 0; price = "0.75"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "500"; onDemand = true; maxDelta = "100"; };
-        "r3.4xlarge" = { number = 0; price = "1.5"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "200"; };
+        "r3.4xlarge" = { instanceType = "i3.4xlarge"; diskSize = "10"; number = 0; price = "1.5"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "200"; };
         "r3.8xlarge" = { instanceType = "i3.8xlarge"; diskSize = "10"; number = 0; price = "3.00"; percentageSpot = "1.0"; percentageQueue = "1.0"; min = "50"; max = "500"; onDemand = true; maxDelta = "100";};
         "i2.xlarge" = { number = 0; price = "0.86"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "200"; min = "75"; };
         "i2.2xlarge" = { number = 0; price = "1.88"; percentageSpot = "1.0"; percentageQueue = "1.0"; max = "500"; min = "75"; onDemand = true; maxDelta = "100";};
@@ -64,7 +64,7 @@ rec {
       inherit (prod) workers;
       spotfleetRole = "arn:aws:iam::202226491534:role/aws-ec2-spot-fleet-role";
       region = {
-          "us-east-1" = { securityGroupsIDs = ["sg-4daea13f"]; Subnets = ["subnet-7451b329" "subnet-883299c3" "subnet-99ce6ea6" "subnet-b850b997" ]; s3-amis = "ami-07f12bcafe2739c59"; ebs-amis = "ami-0cf0222d4f702b5ad";};
+          "us-east-1" = { securityGroupsIDs = ["sg-4daea13f"]; Subnets = ["subnet-7451b329" "subnet-883299c3" "subnet-99ce6ea6" "subnet-b850b997" ]; s3-amis = "ami-01bd7cd6594a1816f"; ebs-amis = "ami-0142865c78c1ca071";};
           "us-east-2" = { securityGroupsIDs = ["sg-c6cbb6ae"]; Subnets = ["subnet-ce3b63b5" "subnet-23c4df4a" "subnet-f01983bd"]; s3-amis = "ami-0144a27be0cbec24f"; ebs-amis = "ami-05f8c29b555607ada"; };
           "us-west-1" = { securityGroupsIDs = ["sg-80237be6"]; Subnets = ["subnet-2e933749" "subnet-8639d3dd"]; s3-amis = "ami-079f6b5eade1739d0"; ebs-amis = "ami-07824e380e946b56d";};
           "us-west-2" = { securityGroupsIDs = ["sg-13527a6e"]; Subnets = ["subnet-406f6268" "subnet-7424102f" "subnet-95149cdd" "subnet-fc43289a"]; s3-amis = "ami-0152d71b5816141c5"; ebs-amis = "ami-0f51cf8d09df06b3d";};
@@ -80,7 +80,7 @@ rec {
       inherit (prod) google-nat-elastic-ip;
       inherit (prod) workers;
       spotfleetRole = "";
-      region = {  
+      region = {
           "us-east-1" = { securityGroupsIDs = []; Subnets = []; s3-amis = ""; ebs-amis = "";};
           "us-east-2" = { securityGroupsIDs = []; Subnets = []; s3-amis = ""; ebs-amis = ""; };
           "us-west-1" = { securityGroupsIDs = []; Subnets = []; s3-amis = ""; ebs-amis = "";};
