b34986450ce600d151e843612082028718a39b82
AhmedSamti <devnull@localhost>
2020-02-17 16:03:41 -0500

    [OPS-27856] Updating amis.nix using 4997369 build id and removed hardcoded region

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,10 +1,8 @@
 {
-  us-east-1.ebs = "ami-0d246ae35f95bd215";
-  us-west-1.ebs = "ami-042064ec223cd0154";
-  us-west-2.ebs = "ami-0fb41c3b695b8a0aa";
-  us-east-2.ebs = "ami-0e7ec0b87551a4b73"; #old
-  us-east-1.s3 = "ami-07184a7650460d6db";
-  us-east-2.s3 = "ami-042fe64f79c92036d"; #old
-  us-west-1.s3 = "ami-ee545b8e"; #old
-  us-west-2.s3 = "ami-ab1ea5d3"; #old
+  us-east-1.ebs = "ami-0db951b09a3f911fa";
+  us-west-1.ebs = "ami-09454d6eef1e4c2e5";
+  us-west-2.ebs = "ami-06a040ef3f3af07e4";
+  us-east-1.s3 = "ami-06479c90d0840441b";
+  us-west-1.s3 = "ami-0241a27e237aef418";
+  us-west-2.s3 = "ami-00488846143ea8c97";
 }
--- nix/environments.nix
+++ nix/environments.nix
@@ -166,7 +166,6 @@ in rec {
 
     region = {
       "us-east-1" = {};
-      "us-east-2" = {};
       "us-west-1" = {};
       "us-west-2" = {};
       "us-central1-f" = {};
