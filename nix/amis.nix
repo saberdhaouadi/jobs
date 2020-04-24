a3fb9aea12b94d59989e82e2e3aa4218d88c5488
AhmedSamti <devnull@localhost>
2020-04-24 19:17:57 -0400

    [OPS-28673] adding amis including pointing to sumologic prod fix

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-0cf943485819c7797";
-  us-west-1.ebs = "ami-0518001d51ebc0263";
-  us-west-2.ebs = "ami-05e6c0f18d81d5fef";
-  us-east-1.s3 = "ami-05dca257719a3df1e";
-  us-west-1.s3 = "ami-0f16ab97a5632f5ca";
-  us-west-2.s3 = "ami-02a8ae3a2f1d2c4ff";
+  us-east-1.ebs = "ami-0ee1836cbb7903772";
+  us-west-1.ebs = "ami-04f622b86398bc95a";
+  us-west-2.ebs = "ami-0f93c514867839e38";
+  us-east-1.s3 = "ami-0a7226ac77bad72cf";
+  us-west-1.s3 = "ami-06c4a7af1c114f1fc";
+  us-west-2.s3 = "ami-02ae584cc0bdae468";
 }
