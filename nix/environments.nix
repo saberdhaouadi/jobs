629adb44a09862f8232134d61ff96ca4c97e356a
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-07-27 17:08:05 +0100

    [OPS-29306] Adding r5ad.4xlarge queue

--- nix/environments.nix
+++ nix/environments.nix
@@ -157,6 +157,15 @@ in rec {
         min = "75";
         diskSize = "10";
       };
+      "r5ad.4xlarge" = {
+        number = 0;
+        price = "1.04";
+        percentageSpot = "1.0";
+        percentageQueue = "1.0";
+        max = "200";
+        min = "75";
+        diskSize = "10";
+      };
       /** Google cloud queues
       * TODO:
       *   - add more instance types
