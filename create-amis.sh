ff7c14dce3825abc9392ccb1d5cb88b74562aeb7
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-08-06 13:13:11 +0100

    Adjusting the new create-amis.sh script

--- create-amis.sh
+++ create-amis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p qemu ec2_ami_tools jq ec2_api_tools awscli -I nixpkgs=channel:nixos-17.09
+#! nix-shell -i bash -p qemu ec2_ami_tools jq ec2_api_tools awscli
 
 # To start with do: nix-shell -p awscli --run "aws configure"
 
--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-04963cdf48c12a91d";
-  us-west-1.ebs = "ami-05cd85683953c7c90";
-  us-west-2.ebs = "ami-07e04daa5c8f907d1";
-  us-east-1.s3 = "ami-0b8234078ba2c590b";
-  us-west-1.s3 = "ami-079301bd7b3aee0ee";
-  us-west-2.s3 = "ami-0233ce577d96d0759";
+  us-east-1.ebs = "ami-043b12d06dc5085db";
+  us-west-1.ebs = "ami-0534b4f7ef280da4d";
+  us-west-2.ebs = "ami-055233d817e0775cf";
+  us-east-1.s3 = "ami-01df757efb2697bb3";
+  us-west-1.s3 = "ami-0885384e8112431b6";
+  us-west-2.s3 = "ami-00fd455fdfb145e77";
 }
