85cf509802ef7257be8fe2ae61830cbbd14bae77
AhmedSamti <deploy-lb-jobs@zeus.predictix.com>
2020-11-04 08:50:43 -0500

    reverting to previous amis

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-038d94c4e813ca2b6";
-  us-west-1.ebs = "ami-0ffe3d91684ee515d";
-  us-west-2.ebs = "ami-070eb511b000a869d";
-  us-east-1.s3 = "ami-02fe12176ad2007fb";
-  us-west-1.s3 = "ami-0a29b7d3073bef090";
-  us-west-2.s3 = "ami-0465b8704ab3cf223";
+  us-east-1.ebs = "ami-043b12d06dc5085db";
+  us-west-1.ebs = "ami-0534b4f7ef280da4d";
+  us-west-2.ebs = "ami-055233d817e0775cf";
+  us-east-1.s3 = "ami-01df757efb2697bb3";
+  us-west-1.s3 = "ami-0885384e8112431b6";
+  us-west-2.s3 = "ami-00fd455fdfb145e77";
 }
