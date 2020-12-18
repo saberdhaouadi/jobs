94368b0a96183a13a1b24a8ac672bb9554c95354
AhmedSamti <deploy-lb-jobs@zeus.predictix.com>
2020-12-18 15:40:57 -0500

    [OPS-30189] fixed amis with builder-config support for different jdks based on platform version

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,9 +1,9 @@
 {
-  us-east-1.ebs = "ami-08176055cd398e6aa";
-  us-west-1.ebs = "ami-099ea7c3d0d110a3a";
-  us-west-2.ebs = "ami-05702a3c1d9167a74";
-  us-east-1.s3 = "ami-000d73ccaa1fc2776";
-  us-west-1.s3 = "ami-0e591a24f4f97dcdb";
-  us-west-2.s3 = "ami-0f8ba10c00d42bbe9";
+  us-east-1.ebs = "ami-0d30f7df3313abf01";
+  us-west-1.ebs = "ami-0935a720954940b59";
+  us-west-2.ebs = "ami-0773b2693f797e20b";
+  us-east-1.s3 = "ami-0021f8e361d11c640";
+  us-west-1.s3 = "ami-02cdc41ecaee15ceb";
+  us-west-2.s3 = "ami-021424edf622fe44c";
 }
 
