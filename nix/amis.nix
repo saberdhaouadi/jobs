ed3d578445cc624c0430fba329144d101709df46
AhmedSamti <deploy-lb-jobs@zeus.predictix.com>
2020-12-21 07:15:49 -0500

    [OPS-30189] amis fix for block device mapping for i3 instances

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,9 +1,8 @@
 {
-  us-east-1.ebs = "ami-0d30f7df3313abf01";
-  us-west-1.ebs = "ami-0935a720954940b59";
-  us-west-2.ebs = "ami-0773b2693f797e20b";
-  us-east-1.s3 = "ami-0021f8e361d11c640";
-  us-west-1.s3 = "ami-02cdc41ecaee15ceb";
-  us-west-2.s3 = "ami-021424edf622fe44c";
+  us-east-1.ebs = "ami-080a4c68211bb0c65";
+  us-west-1.ebs = "ami-0b76e91d93fa53644";
+  us-west-2.ebs = "ami-0a9e7beada731a1af";
+  us-east-1.s3 = "ami-03f30e6edb09cd413";
+  us-west-1.s3 = "ami-0f6ad94d922446635";
+  us-west-2.s3 = "ami-0ff051dce10050a3a";
 }
-
