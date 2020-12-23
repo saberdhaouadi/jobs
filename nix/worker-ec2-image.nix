882e34335c0fbb993ea12be7ac1836ba06f9eb4b
tewfik-ghariani <tewfik.ghariani@infor.com>
2020-12-23 11:23:44 -0500

    [OPS-30027] excluding nvme0n device

--- nix/worker-ec2-image.nix
+++ nix/worker-ec2-image.nix
@@ -80,7 +80,7 @@
       devices=""
       nr=0
       mkdir -p /var/lock/lvm
-      for device in /dev/xvd[bcdef]* /dev/nvme[0-9]n[0-9]; do
+      for device in /dev/xvd[bcdef]* /dev/nvme[1-9]n[0-9]; do
         echo $device
         if [ -e "$device" ]; then
           lvm pvcreate -f $device
