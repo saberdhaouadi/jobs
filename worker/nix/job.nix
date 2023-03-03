5717a279ffe2fac6e5fdef3a89b289054f5c2e0c
Hazem tounsi <hazem.tounsi@infor.com>
2023-03-03 14:46:15 +0100

    Adding Gurobi ISV Key

--- worker/nix/job.nix
+++ worker/nix/job.nix
@@ -37,6 +37,13 @@ in
 
     buildCommand = ''
       export HOME=/tmp/job
+      # make the Gurobi ISV key environment variables available to LB if the
+      # creds file exists.
+      
+      if [[ -f /tmp/gurobi-isv-key ]]; then
+        source /tmp/gurobi-isv-key
+      fi
+      
       function start_lb() 
       {
         export LB_BLOXCOMPILER_SERVER=1;
