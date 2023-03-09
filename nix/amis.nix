4e2484f42367fc31c21446f7bc48c98f0e2c0c8d
Hazem tounsi <hazem.tounsi@infor.com>
2023-03-09 17:46:18 +0100

    adding the new AMIs generated

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-080a4c68211bb0c65";
-  us-west-1.ebs = "ami-0b76e91d93fa53644";
-  us-west-2.ebs = "ami-0a9e7beada731a1af";
-  us-east-1.s3 = "ami-03f30e6edb09cd413";
-  us-west-1.s3 = "ami-0f6ad94d922446635";
-  us-west-2.s3 = "ami-0ff051dce10050a3a";
+  us-east-1.ebs = "ami-0893166d76c0fca22";
+  us-west-1.ebs = "ami-0e8a37c5c7aae0284";
+  us-west-2.ebs = "ami-03e9d93564235906c";
+  us-east-1.s3 = "ami-08ac96bdcce785464";
+  us-west-1.s3 = "ami-002d6a476051f73cf";
+  us-west-2.s3 = "ami-0ad699b23f7fdd573";
 }
--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-04011252a3fdeaf4a";
-  us-west-1.ebs = "ami-00cdbe6ecde027b14";
-  us-west-2.ebs = "ami-0d7918bb6d04de462";
-  us-east-1.s3 = "ami-0a09074051734817a";
-  us-west-1.s3 = "ami-043e64913126f2fd3";
-  us-west-2.s3 = "ami-04fab756d9f698fa1";
+  us-east-1.ebs = "ami-0893166d76c0fca22";
+  us-west-1.ebs = "ami-0e8a37c5c7aae0284";
+  us-west-2.ebs = "ami-03e9d93564235906c";
+  us-east-1.s3 = "ami-08ac96bdcce785464";
+  us-west-1.s3 = "ami-002d6a476051f73cf";
+  us-west-2.s3 = "ami-0ad699b23f7fdd573";
 }
