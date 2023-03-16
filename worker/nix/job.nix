6ea3bbae64129d7cdc82362c79876e033cf5881a
Hazem tounsi <hazem.tounsi@infor.com>
2023-03-16 14:31:40 +0100

    Adding new fix for Gurobi ISV env

--- worker/nix/job.nix
+++ worker/nix/job.nix
@@ -41,7 +41,7 @@ in
       # creds file exists.
       
       if [[ -f /tmp/gurobi-isv-key ]]; then
-        source /tmp/gurobi-isv-key
+        set -a source /tmp/gurobi-isv-key
       fi
       
       function start_lb() 
