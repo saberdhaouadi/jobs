9c66e8d00432aff1c3937be4869a5ff5607a9f36
Hazem tounsi <hazem.tounsi@infor.com>
2023-03-16 18:05:15 +0100

    New images

--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-04eb8a0ed79acad31";
-  us-west-1.ebs = "ami-0c9542767ce2928a3";
-  us-west-2.ebs = "ami-01970b8ecd8520d49";
-  us-east-1.s3 = "ami-0d28dfc8c82f19424";
-  us-west-1.s3 = "ami-0d34a9abd136c4bf2";
-  us-west-2.s3 = "ami-07496868bde21c3b9";
+  us-east-1.ebs = "ami-05fc431b8195d802b";
+  us-west-1.ebs = "ami-0e73d60c6459fb3c6";
+  us-west-2.ebs = "ami-05ad868f0ad31c10a";
+  us-east-1.s3 = "ami-00563046b93ea6884";
+  us-west-1.s3 = "ami-0dc6ed39cdb9f1f7c";
+  us-west-2.s3 = "ami-0d3095826be8f2be1";
 }
