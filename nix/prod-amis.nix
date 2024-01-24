cbc98386e6b23aa84cb459500baee0303a1d39c4
Hazem Tounsi <hazem.tounsi@infor.com>
2024-01-24 12:16:24 +0000

    prod-amis.nix edited online with Bitbucket

--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-05fc431b8195d802b";
-  us-west-1.ebs = "ami-0e73d60c6459fb3c6";
-  us-west-2.ebs = "ami-05ad868f0ad31c10a";
-  us-east-1.s3 = "ami-00563046b93ea6884";
-  us-west-1.s3 = "ami-0dc6ed39cdb9f1f7c";
-  us-west-2.s3 = "ami-0d3095826be8f2be1";
+  us-east-1.ebs = "ami-0b54b548613f9ec42";
+  us-west-1.ebs = "ami-02db0273b9aa1b4e6";
+  us-west-2.ebs = "ami-06726f47658b37924";
+  us-east-1.s3 = "ami-06e65fdcd72f78ab3";
+  us-west-1.s3 = "ami-067f6b14bfbdd09cc";
+  us-west-2.s3 = "ami-097c807b4b29542cd";
 }
