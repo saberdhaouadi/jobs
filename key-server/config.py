992760287c69cda953b9054abdbf3a4bd9a0a705
Wes Hunter <wes.hunter@logicblox.com>
2024-03-26 10:22:57 -0400

    typo in version number

--- key-server/config.py
+++ key-server/config.py
@@ -58,7 +58,7 @@ classpath = [
   '$(lb_web)/lib/java/google-api-client-1.30.3.jar',
   '$(lb_web)/lib/java/google-http-client-1.32.0.jar',
   '$(lb_web)/lib/java/google-http-client-jackson2-1.32.0.jar',
-  '$(lb_web)/lib/java/google-oauth-client-1.30.3.jar',
+  '$(lb_web)/lib/java/google-oauth-client-1.33.3.jar',
   '$(lb_web)/lib/java/grpc-context-1.22.1.jar',
   '$(lb_web)/lib/java/j2objc-annotations-1.3.jar',
   '$(lb_web)/lib/java/opencensus-api-0.24.0.jar',
