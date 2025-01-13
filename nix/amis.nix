ffc767b6e3cfd43facf56b860dd954edf9d2c366
Saber Dhaouadi <saber.dhaouadi@infor.com>
2025-01-13 15:56:15 +0000

    amis.nix edited online with Bitbucket

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,6 +1,6 @@
 {
-  us-east-1.ebs = "ami-05fc431b8195d802b";
-  us-west-1.ebs = "ami-0e73d60c6459fb3c6";
+  us-east-1.ebs = ""ami-0d7649229b13faaf4;
+  us-west-1.ebs = "ami-0a82b863eb411a424";
   us-west-2.ebs = "ami-05ad868f0ad31c10a";
   us-east-1.s3 = "ami-00563046b93ea6884";
   us-west-1.s3 = "ami-0dc6ed39cdb9f1f7c";
