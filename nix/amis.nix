b978d86b753b8bc8be561575a92225d95fef8c75
saber.dhaouadi <saber.dhaouadi@infor.com>
2025-01-13 18:47:03 +0100

    testing the new 2025 Gurobi ISV key

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-6dc1c717";
-  us-west-1.ebs = "ami-fb56599b";
-  us-west-2.ebs = "ami-681ea510";
-  us-east-1.s3 = "ami-42cacc38";
+  us-east-1.ebs = "ami-0b9ccd6debb8d2799";
+  eu-west-1.ebs = "ami-0a82b863eb411a424";
+  us-west-2.ebs = "ami-0370268fd86605f72";
+  us-east-1.s3 = "ami-05171bc13c19d2d71";
   us-west-1.s3 = "ami-ee545b8e";
   us-west-2.s3 = "ami-ab1ea5d3";
 }
