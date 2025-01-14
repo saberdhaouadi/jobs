9863682953486669ceae241f664a3dae0307b4eb
saber.dhaouadi <saber.dhaouadi@infor.com>
2025-01-14 12:51:56 +0100

    adding new amis with 2025 ISV key

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,8 @@
 {
   us-east-1.ebs = "ami-0b9ccd6debb8d2799";
-  eu-west-1.ebs = "ami-0a82b863eb411a424";
-  us-west-2.ebs = "ami-0370268fd86605f72";
-  us-east-1.s3 = "ami-05171bc13c19d2d71";
-  us-west-1.s3 = "ami-ee545b8e";
-  us-west-2.s3 = "ami-ab1ea5d3";
+  eu-west-1.ebs = "ami-0e2bd58c9e5dcab80";
+  us-west-2.ebs = "ami-023c6c3b658e042f2";
+  us-east-1.s3 = "ami-0ef16326de87fbc1e";
+  us-west-1.s3 = "ami-01dfd9939093a7121";
+  us-west-2.s3 = "ami-040d0ce04faadca59";
 }
