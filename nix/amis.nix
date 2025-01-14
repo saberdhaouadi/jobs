bfe8f74d4ccb601f51752b5bfc4a20712828776b
saber.dhaouadi <saber.dhaouadi@infor.com>
2025-01-14 19:16:59 +0100

    new amis with 2025 ISV Gurobi key

--- nix/amis.nix
+++ nix/amis.nix
@@ -1,8 +1,9 @@
 {
-  us-east-1.ebs = "ami-0c4e5317ef68dc188";
-  us-west-1.ebs = "ami-03f367866e43b780e";
-  us-west-2.ebs = "ami-00b7669d76acbd087";
-  us-east-1.s3 = "ami-0a7f192e06405e422";
-  us-west-1.s3 = "ami-09abff34e2a215aba";
-  us-west-2.s3 = "ami-00b6ee7a7083c0975";
+  us-east-1.ebs = "ami-0b9ccd6debb8d2799";
+  us-west-1.ebs = "ami-02862a13238aa513b";
+  us-west-2.ebs = "ami-0370268fd86605f72";
+  us-east-1.s3 = "ami-05171bc13c19d2d71";
+  us-west-1.s3 = "ami-0918532f6a526d288";
+  us-west-2.s3 = "ami-0d81b494adb4b9874";
+
 }
