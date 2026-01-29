554cc3d5da93a06481163a859759def3cb58b19b
Saber Dhaouadi <dhaouadis@gmail.com>
2026-01-29 17:27:40 +0100

    Update AMI IDs for various regions

--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-0b9ccd6debb8d2799";
-  us-west-1.ebs = "ami-02862a13238aa513b";
-  us-west-2.ebs = "ami-0370268fd86605f72";
-  us-east-1.s3 = "ami-05171bc13c19d2d71";
-  us-west-1.s3 = "ami-0918532f6a526d288";
-  us-west-2.s3 = "ami-0d81b494adb4b9874";
+  us-east-1.ebs = "ami-01d7d65975b009ad3";
+  us-west-1.ebs = "ami-07b3dfad41e2f216b";
+  us-west-2.ebs = "ami-0d95d3b791a5a34b6";
+  us-east-1.s3 = "ami-08a18314a5d02cc49";
+  us-west-1.s3 = "ami-047f35fa52b5e4a26";
+  us-west-2.s3 = "ami-026f4206b30c64c24";
 }
