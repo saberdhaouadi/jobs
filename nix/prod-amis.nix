b0c9362bbeaea204a2a6d73f1cee0d8307c0d36b
Aliaksandr Halouchyk <alhalouchyk@gmail.com>
2026-01-31 02:23:38 +0300

    Update prod-amis.nix
    Gurobi 9 new ISV key

--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-01d7d65975b009ad3";
-  us-west-1.ebs = "ami-07b3dfad41e2f216b";
-  us-west-2.ebs = "ami-0d95d3b791a5a34b6";
-  us-east-1.s3 = "ami-08a18314a5d02cc49";
-  us-west-1.s3 = "ami-047f35fa52b5e4a26";
-  us-west-2.s3 = "ami-026f4206b30c64c24";
+  us-east-1.ebs = "ami-0bd7642e653b72e2b";
+  us-west-1.ebs = "ami-021f21b658796fb9f";
+  us-west-2.ebs = "ami-03a010754f4c31f86";
+  us-east-1.s3 = "ami-005a4e008a1bf4009";
+  us-west-1.s3 = "ami-0c26a5caa3e2f5983";
+  us-west-2.s3 = "ami-0c484f79ff5a7bc2d";
 }
