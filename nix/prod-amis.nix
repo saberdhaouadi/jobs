6a2d53b98f2c587960122002bf7aa193e4cc8b56
Saber Dhaouadi <dhaouadis@gmail.com>
2026-01-31 12:54:53 +0100

    change amis of january 30

--- nix/prod-amis.nix
+++ nix/prod-amis.nix
@@ -1,8 +1,8 @@
 {
-  us-east-1.ebs = "ami-0b9ccd6debb8d2799";
-  us-west-1.ebs = "ami-02862a13238aa513b";
-  us-west-2.ebs = "ami-0370268fd86605f72";
-  us-east-1.s3 = "ami-05171bc13c19d2d71";
-  us-west-1.s3 = "ami-0918532f6a526d288";
-  us-west-2.s3 = "ami-0d81b494adb4b9874";
+  us-east-1.ebs = "ami-0bd7642e653b72e2b";
+  us-west-1.ebs = "ami-021f21b658796fb9f";
+  us-west-2.ebs = "ami-03a010754f4c31f86";
+  us-east-1.s3 = "ami-005a4e008a1bf4009";
+  us-west-1.s3 = "ami-0c26a5caa3e2f5983";
+  us-west-2.s3 = "ami-0c484f79ff5a7bc2d";
 }
